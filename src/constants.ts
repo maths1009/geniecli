d7785dd6083c34bf026d0e759523d1d8e7b2aeb5
Mathis <lb.brangeon@gmail.com>
2024-06-16 16:39:25 +0200

    🔧envVar constante

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const envVars: Record<ProjectType, Record<string, string>> = {
+  react: {
+    PORT: "3000",
+  },
+  vue: {
+    PORT: "3000",
+  },
+  express: {
+    PORT: "8080",
+  },
+};
